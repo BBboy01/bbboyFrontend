7ecf3b61122e548217677b29d11c1d90d833523b
BBboy01 <1156678002@qq.com>
2021-05-16 20:09:47 +0800

    .

--- src/views/BackStage.vue
+++ src/views/BackStage.vue
@@ -6,39 +6,58 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, reactive, nextTick } from 'vue'
 import * as echarts from 'echarts'
+import { getAllNote } from '../requests/getNote'
 
 
 export default {
   setup () {
     // 获取标签容器
     let indexChart = ref(null)
+    let allNoteVists = ref([])
+
     onMounted(async () => {
-      // 初始化
+      // 获取数据
+      allNoteVists.value = await getAllNote()
+      // 初始化图表
       let barChart = echarts.init(indexChart.value)
-      // barChartOptions 配置项，推荐放在外部引入。
-      barChart.setOption({
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [{
-          data: [120, 200, 150, 80, 70, 110, 130],
-          type: 'bar',
-          showBackground: true,
-          backgroundStyle: {
-            color: 'rgba(180, 180, 180, 0.2)'
-          }
-        }]
+
+      nextTick(() => {
+        let titles = allNoteVists.value.reduce((pre, cur) => {
+          pre.push(cur.title)
+          return pre
+        }, [])
+        let visits = allNoteVists.value.reduce((pre, cur) => {
+          pre.push(cur.visits)
+          return pre
+        }, [])
+
+        // barChartOptions 配置项，推荐放在外部引入。
+        barChart.setOption({
+          xAxis: {
+            type: 'category',
+            data: titles
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            data: visits,
+            type: 'bar',
+            showBackground: true,
+            backgroundStyle: {
+              color: 'rgba(180, 180, 180, 0.2)'
+            }
+          }]
+        })
       })
+
+      //当浏览器尺寸变化时自适应大小, 不用的话不会自适应大小。
       window.onresize = function () {
-        //自适应大小, 不用的话不会自适应大小。
         barChart.resize()
       }
+
     })
 
     return {
